9d0a7bb12926e122f2d0037d495d213cba1d6c6d
NipunaDassanayake <nipuna920@gmail.com>
2025-03-25 16:56:32 +0530

    did some updates in user management

---
+++ src/main/java/com/melkart_api/melkart_api/controller/dto/request/UserRequestDTO.java
@@ -0,0 +1,4 @@
+package com.melkart_api.melkart_api.controller.dto.request;
+
+public class UserRequestDTO {
+}
---
+++ src/main/java/com/melkart_api/melkart_api/service/impl/UserServiceImpl.java
@@ -0,0 +1,4 @@
+package com.melkart_api.melkart_api.service.impl;
+
+public class UserServiceImpl {
+}
